823242124b0e6b215f606ce556ee69b25b05140e
RazzJT <rasmusjt77@gmail.com>
2024-01-22 09:38:17 +0100

    Circle program

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
   createCanvas(400, 400);
+
 }
 
 function draw() {
   background(220);
+}
+
+function RandomCirleShit() {
+  x = random(1,10);
+  y = random(1,10);
+  CreateCircle();
+}
+
+function CreateCircle(x,y) {
+  fill("red");
+  circle(x,y);
 }
