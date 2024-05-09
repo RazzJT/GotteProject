a02e540ae8b848fd08b02212582ae590656c4c55
Torp <rasmusjt77@gmail.com>
2024-05-09 19:27:11 +0200

    Kommenteret kode

--- sketch.js
+++ sketch.js
@@ -77,27 +77,32 @@ function draw() { // Styrer spillets hovedlogik baseret på menu tilstand med en
 
 
 function runGame() {  
-  background(0);
-  player.show();
-  player.move();
+  background(0); // Sætter baggrundsfarven til sort
+  player.show(); // Viser spilleren på skærmen
+  player.move(); // Opdaterer spillerens position baseret på input
 
-  let edge = false;
+  let edge = false; // En boolsk variabel til at tjekke om en invader rører kanten af skærmen
 
-  enemyShoot();
+  enemyShoot(); // Funktion der håndterer når fjender (invaders) skyder
 
-   for (let bullet of bullets) {
-    bullet.show();
-    bullet.move();
+  // Gennemgår hver bullet i spillerens bullets-array
+  for (let bullet of bullets) {
+    bullet.show(); // Viser kuglen
+    bullet.move(); // Bevæger kuglen
 
+    // Fjerner kuglen hvis den er uden for skærmens top
     if (bullet.y < -10) {
       bullets.splice(bullets.indexOf(bullet), 1);
       console.log("bullet removed");
     }
+
+    // Tjekker kollision mellem spillerens kugler og invaders
     for (let i = invaders.length - 1; i >= 0; i--) {
       if (bullet.hits(invaders[i])) {
-        score += 10;
-        invaders.splice(i, 1);
-        bullets.splice(bullets.indexOf(bullet), 1);
+        score += 10; // Tildeler point for at ramme en invader
+        invaders.splice(i, 1); // Fjerner den ramte invader fra arrayet
+        bullets.splice(bullets.indexOf(bullet), 1); // Fjerner kuglen der ramte invaderen
+        // Tildeler spilleren guld med en chance på 1 ud af 4
         if(random([1,2,3,4]) == 1) {
           gold += 10;
         }
@@ -105,36 +110,39 @@ function runGame() {
     }
   }
 
+  // Gennemgår hver invader i invaders-arrayet
   for (let invader of invaders) {
-    invader.show();
-    invader.move(invaderDirection);
+    invader.show(); // Viser invaderen
+    invader.move(invaderDirection); // Bevæger invaderen i den aktuelle retning
+    // Tjekker om en invader rører kanten og ændrer retning hvis sandt
     if (invader.x > width - invader.w || invader.x < invader.w) {
       edge = true;
     }
-    if (invader.y + invader.w >= height - 30) { // `30` can be adjusted based on where you want the threshold
-      currentMenu = Menu.restart 
-      highScore = max(highScore, score);
+    // Tjekker om invaders når bunden af skærmen og slutter spillet hvis sandt
+    if (invader.y + invader.w >= height - 30) {
+      currentMenu = Menu.restart;
+      highScore = max(highScore, score); // Opdaterer highscore hvis nødvendigt
     }
   }
 
-  
+  // Gennemgår hver fjende kugle i enemyBullets-arrayet
   for (let i = enemyBullets.length - 1; i >= 0; i--) {
-    enemyBullets[i].show();
-    enemyBullets[i].move();
-  
+    enemyBullets[i].show(); // Viser kuglen
+    enemyBullets[i].move(); // Bevæger kuglen
+
+    // Tjekker om en kugle rammer spilleren og slutter spillet hvis sandt
     if (enemyBullets[i].hits(player)) {
       console.log('Player hit!');
-      currentMenu = Menu.restart; 
+      currentMenu = Menu.restart;
     }
-  
+
+    // Fjerner kuglen hvis den går uden for bunden af skærmen
     if (enemyBullets[i].y > 510) {
-      console.log(enemyBullets);
       enemyBullets.splice(i, 1);
-      console.log("Enemy bullets removed");
-      console.log(player);
     }
   }
 
+  // Ændrer retning af alle invaders og flytter dem nedad hvis en rører kanten
   if (edge) {
     invaderDirection *= -1;
     for (let invader of invaders) {
@@ -142,10 +150,13 @@ function runGame() {
     }
   }
   
+  // Hvis der ikke er flere invaders, skifter spillet til shop-menuen og starter en ny runde
   if (invaders.length <= 0) {
     currentMenu = Menu.shop;
-    roundCount++
+    roundCount++;
   }
+
+
 
 
   // Viser text pg score
@@ -161,81 +172,90 @@ function runGame() {
 
 
 function displayStartScreen() {
-  background(50);
-  textSize(40);
-  fill(255);
-  textAlign(CENTER);
-  text("Highscore " + highScore, 500, 50)
-  text("Press Start to Play", 500, 300)
+  background(50); // Sætter baggrundsfarven til en mørkegrå
+  textSize(40); // Sætter størrelsen på teksten til 40
+  fill(255); // Sætter tekstfarven til hvid
+  textAlign(CENTER); // Centrerer teksten
+  text("Highscore " + highScore, 500, 50); // Viser highscore på skærmen
+  text("Press Start to Play", 500, 300); // Instruerer spilleren om at starte spillet
 
-
-  startGameButton.show();
+  startGameButton.show(); // Viser startknap
 }
 
 function displayShop() {
-  background(50);
-  textSize(20);
-  fill(255);
-  textAlign(RIGHT);
+  background(50); // Sætter baggrundsfarven til en mørkegrå
+  textSize(20); // Sætter tekststørrelsen til 20
+  fill(255); // Sætter tekstfarven til hvid
+  textAlign(RIGHT); // Teksten vil blive højrejusteret
+  // Viser forskellige spilinformationer på skærmen
   text("Score: " + score, 950, 30);
-  text("Highscore: "  + highScore, 950, 60);
-  text("Round: " + roundCount, 100, 30 )
-  text("Gold: " + gold, 100, 60)
-  text("Upgrade Cost: " + upgradeCost + " Gold", 400, 210); // Display upgrade cost
-  continueGameButton.show();
-  upgradeShootSpeed.show();
+  text("Highscore: " + highScore, 950, 60);
+  text("Round: " + roundCount, 100, 30);
+  text("Gold: " + gold, 100, 60);
+  text("Upgrade Cost: " + upgradeCost + " Gold", 400, 210);
+  
+  continueGameButton.show(); // Viser knappen for at fortsætte spillet
+  upgradeShootSpeed.show(); // Viser knappen for at opgradere skudhastighed
 }
 
+
 function displayGameOverScreen() {
-  background(50);
-  textSize(40);
-  fill(255);
-  textAlign(CENTER);
+  background(50); // Sætter baggrundsfarven til en mørkegrå
+  textSize(40); // Sætter tekststørrelsen til 40
+  fill(255); // Sætter tekstfarven til hvid
+  textAlign(CENTER); // Teksten vil blive centreret
+  // Viser game over beskeder
   text("Game Over", width / 2, height / 2);
   text("Score: " + score, width / 2, height / 2 + 50);
   text("High Score: " + highScore, width / 2, height / 2 + 100);
-  restartButton.show();
+  
+  restartButton.show(); // Viser genstartsknap
 }
 
+
 function keyPressed() {
-  if (currentMenu = Menu.game) {
+  if (currentMenu = Menu.game) { // Tjekker om spillet er i gang
     if (keyCode === RIGHT_ARROW) {
-      player.setDirection(1);
+      player.setDirection(1); // Flyt spilleren til højre
     } else if (keyCode === LEFT_ARROW) {
-      player.setDirection(-1);
-    } else if (keyCode === 32) { // Spacebar to shoot
+      player.setDirection(-1); // Flyt spilleren til venstre
+    } else if (keyCode === 32) { // Spacebar til at skyde
       if (millis() - lastShootTime > shootCooldown) {
-        bullets.push(new Bullet(player.x, height - 20));
-        lastShootTime = millis();
+        bullets.push(new Bullet(player.x, height - 20)); // Skab en ny kugle
+        lastShootTime = millis(); // Opdaterer tidspunktet for sidste skud
       }
     }
   }
 }
 
+
 function enemyShoot() {
-  let currentTime = millis();
-  if (currentTime - lastEnemyShootTime > enemyShootInterval) {
+  let currentTime = millis(); // Gemmer den aktuelle tid
+  if (currentTime - lastEnemyShootTime > enemyShootInterval) { // Tjekker om det er tid til at invaders skal skyde
     if (invaders.length > 0) {
-      let shooter = random(invaders); // select a random invader to shoot
-      enemyBullets.push(new EnemyBullet(shooter.x + shooter.w / 2, shooter.y + shooter.w));
-      lastEnemyShootTime = currentTime; // reset the last shoot time
+      let shooter = random(invaders); // Vælger en tilfældig invader
+      enemyBullets.push(new EnemyBullet(shooter.x + shooter.w / 2, shooter.y + shooter.w)); // Opretter en ny kugle fra invaderen
+      lastEnemyShootTime = currentTime; // Nulstiller tidspunktet for sidste skud
     }
   }
 }
 
+
 function startGame() {
-  // Reset all variables
+  // Nulstiller spilvariabler
   score = 0;
   roundCount = 1;
   enemyShootInterval = 3000;
 
+  // Rydder arrays for spilobjekter
   invaders = [];
   bullets = [];
   enemyBullets = [];
-  currentMenu = Menu.game
-  restartButton.hide();
-  startGameButton.hide();
-  player = new Player();
+  currentMenu = Menu.game; // Sætter spilmenuen til aktivt spil
+  restartButton.hide(); // Skjuler genstartsknap
+  startGameButton.hide(); // Skjuler startknap
+  player = new Player(); // Opretter en ny spiller
+  // Fylder invaders-array med nye invader objekter
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 5; j++) {
       invaders.push(new Invader(i * 50 + 50, j * 50 + 20, j + 1));
@@ -243,55 +263,59 @@ function startGame() {
   }
 }
 
+
 function newRound() {
+  // Forbereder en ny runde ved at rydde og genopbygge spilarrays
   invaders = [];
   bullets = [];
-  currentMenu = Menu.game
-  invaderSpeed = invaderSpeed + roundCount * 0.2
-  enemyShootInterval = enemyShootInterval - roundCount * 100
+  currentMenu = Menu.game; // Sætter spilmenuen til aktivt spil
+  invaderSpeed = invaderSpeed + roundCount * 0.2; // Øger invaders' hastighed baseret på runden nummer
+  enemyShootInterval = enemyShootInterval - roundCount * 100; // Reducerer intervallet mellem invaders' skud
+  // Genopbygger invaders-array med nye invader objekter
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 5; j++) {
       invaders.push(new Invader(i * 50 + 50, j * 50 + 20, j + 1));
     }
   }
-  continueGameButton.hide()
-  upgradeShootSpeed.hide()
+  continueGameButton.hide(); // Skjuler fortsæt-knappen
+  upgradeShootSpeed.hide(); // Skjuler opgraderingsknappen
 }
+
 
 
 
 function keyReleased() {
   if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
-    player.setDirection(0);
+    player.setDirection(0); // Nulstiller spillerens bevægelsesretning når pilen slippes
   }
 }
 
 class Player {
   constructor() {
-    this.x = width / 2;
-    this.y = 500;
-    this.xdir = 0;
-    this.velocity = 0; 
-    this.speed = 0.7; 
-    this.r = 10;
+    this.x = width / 2; // Spillerens startposition i x-aksen (midten af skærmen)
+    this.y = 500; // Spillerens startposition i y-aksen
+    this.xdir = 0; // Retning af spillerens bevægelse, 0 betyder stillestående
+    this.velocity = 0; // Spillerens hastighed, startværdi er 0
+    this.speed = 0.7; // Hastighedsfaktor for bevægelse
+    this.r = 10; // Radius af spilleren til kollisionsdetektion
   }
 
   show() {
-    fill(255);
-    rectMode(CENTER);
-    rect(this.x, height - 10, 20, 20);
+    fill(255); // Sætter farven på spilleren til hvid
+    rectMode(CENTER); // Sætter tegnemode til centeret om koordinater
+    rect(this.x, height - 10, 20, 20); // Tegner spilleren som en firkant
   }
 
   setDirection(dir) {
-    this.xdir = dir; 
+    this.xdir = dir; // Ændrer retning baseret på input (dir)
   }
 
   move() {
-    this.velocity += this.xdir * this.speed;
-    this.x += this.velocity; 
-    this.velocity *= 0.9; 
+    this.velocity += this.xdir * this.speed; // Opdaterer hastighed baseret på retning og hastighedsfaktor
+    this.x += this.velocity; // Flytter spilleren langs x-aksen
+    this.velocity *= 0.9; // Anvender en dæmpning på hastigheden for at simulere friktion
 
-    // Constrain player within the canvas
+    // Begrænser spillerens bevægelse til inden for canvas-rammerne
     this.x = constrain(this.x, this.r, width - this.r);
   }
 }
@@ -299,72 +323,76 @@ class Player {
 
 class Invader {
   constructor(x, y, row) {
-    this.x = x;
-    this.y = y;
-    this.w = 20; // Radius for invaderen
-    this.row = row; // Række for invaderen
+    this.x = x; // Start x-position for invaderen
+    this.y = y; // Start y-position for invaderen
+    this.w = 20; // Størrelse af invaderen (bredde og højde)
+    this.row = row; // Rækken hvor invaderen er placeret
   }
 
   show() {
-    fill("red");
-    rect(this.x, this.y, this.w, this.w);
-    fill("white")
-     rect(this.x+-5, this.y+-2,5,5)
-    rect(this.x+5, this.y+-2,5,5)
-    fill("white")
-    ellipse(this.x, this.y + 6,15,5)
+    fill("red"); // Farver invaderen rød
+    rect(this.x, this.y, this.w, this.w); // Tegner invaderen som en firkant
+    fill("white");
+    rect(this.x - 5, this.y - 2, 5, 5); // Tegner det venstre øje
+    rect(this.x + 5, this.y - 2, 5, 5); // Tegner det højre øje
+    fill("white");
+    ellipse(this.x, this.y + 6, 15, 5); // Tegner invaderens mund
   }
 
   move(direction) {
-    this.x += direction * invaderSpeed; // Bevægelse baseret på retning
+    this.x += direction * invaderSpeed; // Flytter invaderen langs x-aksen baseret på retning og hastighed
   }
 
   shiftDown() {
-    this.y += 40; // Bevægelse nedad
+    this.y += 40; // Flytter invaderen nedad ved kanten af skærmen
   }
 }
 
-// Kugleklasse
+
+
 class Bullet {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x; // Start x-position for kuglen
+    this.y = y; // Start y-position for kuglen
   }
 
   show() {
-    fill(50, 0, 200);
-    ellipse(this.x, this.y, 4, 10);
+    fill(50, 0, 200); // Farver kuglen blå
+    ellipse(this.x, this.y, 4, 10); // Tegner kuglen som en ellipse
   }
 
   move() {
-    this.y -= 5;
+    this.y -= 5; // Bevæger kuglen opad
   }
 
   hits(invader) {
-    let d = dist(this.x, this.y, invader.x, invader.y);
-    return d < 20;
+    let d = dist(this.x, this.y, invader.x, invader.y); // Beregner afstanden til invaderen
+    return d < 20; // Returnerer true hvis kuglen rammer invaderen
   }
 }
+
+
 
 // Invader kugleklasse
 class EnemyBullet {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.r = 5; // radius of the projectile
+    this.x = x; // Start x-position for fjendens kugle
+    this.y = y; // Start y-position for fjendens kugle
+    this.r = 5; // Radius af fjendens kugle
   }
 
   show() {
-    fill(255, 0, 0); // red color for enemy bullet
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    fill(255, 0, 0); // Farver fjendens kugle rød
+    ellipse(this.x, this.y, this.r * 2, this.r * 2); // Tegner kuglen som en ellipse
   }
 
   move() {
-    this.y += 3; // adjust speed as necessary
+    this.y += 3; // Bevæger kuglen nedad
   }
 
   hits(player) {
-    let d = dist(this.x, this.y, player.x, player.y);
-    return d < 20;  
+    let d = dist(this.x, this.y, player.x, player.y); // Beregner afstanden til spilleren
+    return d < 20; // Returnerer true hvis kuglen rammer spilleren
   }
 }
+  
